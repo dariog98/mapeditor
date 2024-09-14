4e6daa0ae2f27c0a28c9cbdeec1ae261586d0bd6
Gonzalez Dario <darioggonzalez98@gmail.com>
2024-09-14 03:22:46 -0300

    refactored home page

---
+++ src/hooks/useFile.jsx
@@ -0,0 +1,37 @@
+import { useEditorContext } from '../components/providers/EditorProvider'
+
+const useFile = ({ handleCurrentTab }) => {
+    const editor = useEditorContext()
+
+    const handleNewFile = () => {
+
+    }
+
+    const handleLoadFile = (event) => {
+        const files = event.target.files
+
+        if (files && files.length) {
+            const file = files[0]
+
+            const fileReader = new FileReader()
+            fileReader.onload = (event) => {
+                const text = event.target.result
+                const data = JSON.parse(text)
+                editor.createNewMap(data?.id ?? 'unknow')
+                Object.keys(data.stages).map(stageId => {
+                    const stage = data.stages[stageId]
+                    editor.addNewStage(stageId, stage.name, stage.backgroundColor, stage.gridSize.width, stage.gridSize.height, stage.tilesetId, stage.layers)
+                })
+                handleCurrentTab('Editor')
+            }
+            fileReader.readAsText(file)
+        }
+    }
+
+    return {
+        handleNewFile,
+        handleLoadFile
+    }
+}
+
+export default useFile
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,35 +1,14 @@
 import { faFileCirclePlus, faFilePen } from '@fortawesome/free-solid-svg-icons'
 import { Button } from '../components/basics'
 import { useRef } from 'react'
-import { useEditorContext } from '../components/providers/EditorProvider'
+import { useFile } from '../hooks'
 
 const Home = ({ handleCurrentTab }) => {
-    const editor = useEditorContext()
+    const { handleLoadFile } = useFile({ handleCurrentTab })
     const hiddenInputFile = useRef()
 
     const handleLoadMapButton = () => {
         hiddenInputFile.current.click()
-    }
-
-    const handleInputFile = (event) => {
-        const files = event.target.files
-
-        if (files && files.length) {
-            const file = files[0]
-
-            const fileReader = new FileReader()
-            fileReader.onload = (event) => {
-                const text = event.target.result
-                const data = JSON.parse(text)
-                editor.createNewMap(data?.id ?? 'unknow')
-                Object.keys(data.stages).map(stageId => {
-                    const stage = data.stages[stageId]
-                    editor.addNewStage(stageId, stage.name, stage.backgroundColor, stage.gridSize.width, stage.gridSize.height, stage.tilesetId, stage.layers)
-                })
-                handleCurrentTab('Editor')
-            }
-            fileReader.readAsText(file)
-        }
     }
 
     return (
@@ -50,7 +29,7 @@ const Home = ({ handleCurrentTab }) => {
                     handleOnClick={handleLoadMapButton}
                 />
 
-                <input className='d-none' type='file' ref={hiddenInputFile} accept='.json' onChange={handleInputFile}/>
+                <input className='d-none' type='file' ref={hiddenInputFile} accept='.json' onChange={handleLoadFile}/>
             </div>
         </div>
     )
